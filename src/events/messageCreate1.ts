d80fd1fbd9a8490c80204e8465959978e8c2c26a
dragongoose <tylerjseverino@gmail.com>
2022-06-22 23:24:33 -0400

    make a embed of video info

--- src/events/messageCreate1.ts
+++ src/events/messageCreate1.ts
@@ -1,7 +1,8 @@
-import {Message, MessageReaction, MessageAttachment} from 'discord.js';
+import {Message, MessageAttachment, MessageEmbed} from 'discord.js';
 import {client} from '../../index';
 import {determineUrl, downloadVideo} from '../func';
 import log4js from 'log4js';
+import ytdl from 'ytdl-core';
 
 log4js.configure({
   appenders: {messageCreate: {type: 'file', filename: 'messageCreate.log'}},
@@ -20,51 +21,6 @@ client.on('messageCreate', async (message: Message) => {
 
   if (urlType === 'unknown') return logger.debug('ignoring, unknown url');
 
-  if (urlType === 'youtube') {
-    await message.react('🎥');
-    await message.react('❌');
-    logger.debug('added reactions');
-
-    const collector = message.createReactionCollector({time: 60000});
-
-    collector.on('collect', async (reaction: MessageReaction) => {
-      switch (reaction.emoji.name) {
-        case '🎥': {
-          const msg = await message.reply('Downloading video...');
-          logger.debug('downloading video');
-
-          const video = await downloadVideo(message.content, msg).catch(err => {
-            logger.error(err);
-            msg.edit('Error downloading video');
-          });
-
-          console.log(video);
-
-          const file = new MessageAttachment(video + '/');
-
-          msg.edit({content: 'video', files: [file]});
-
-          collector.stop();
-          logger.debug('stopping collector');
-          break;
-        }
-
-        case '❌':
-          // remove of the 2 emojis
-          collector.stop();
-          logger.debug('stopping collector');
-          break;
-      }
-    });
-
-    collector.on('end', async () => {
-      logger.debug('collector ended');
-      await message.reactions.cache.get('🎥')!.remove();
-      await message.reactions.cache.get('❌')!.remove();
-      logger.debug('removed reactions');
-    });
-  }
-
   if (urlType === 'youtubeShorts' || urlType === 'tiktok') {
     const msg = await message.reply('Working...');
     logger.debug('getting video');
@@ -72,15 +28,34 @@ client.on('messageCreate', async (message: Message) => {
     // Convert YT short url to long url
     const newUrl = message.content.replace('shorts/', 'watch?v=');
 
-    const video = await downloadVideo(newUrl, msg).catch(err => {
+    const video = await downloadVideo(newUrl).catch(err => {
       logger.error(err);
       msg.edit('Whoops.. something went wrong');
     });
 
+    // use ytdl-core to get the info about the video
+    const info = await ytdl.getInfo(newUrl);
+    logger.debug(`info: ${JSON.stringify(info)}`);
+
+    // get some info about the video
+    const title = info.videoDetails.title;
+    const author = info.videoDetails.author.name;
+
+    // create an embed
+    const embed = new MessageEmbed()
+      .setTitle(title)
+      .setAuthor({
+        name: author,
+        iconURL: info.videoDetails.author.thumbnails![0].url,
+      })
+      .setColor('#0099ff')
+      .setDescription(
+        `${info.videoDetails.likes} likes \n ${info.videoDetails.viewCount} views`
+      );
+
     const file = new MessageAttachment(video + '/');
-    msg.edit({content: 'video', files: [file]});
+    msg.edit({embeds: [embed], files: [file]});
 
     //TODO Delete video after it's been sent
-    //TODO Send the discord attachment URL to mongodb
   }
 });
