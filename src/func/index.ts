838a912fa99776b04dd9f94e114d7dd1e6018ffb
dragongoose <tylerjseverino@gmail.com>
2022-06-22 23:24:19 -0400

    remove everything but youtube short support

--- src/func/index.ts
+++ src/func/index.ts
@@ -1,27 +1,11 @@
 import * as youtubedl from 'yt-dlp-exec';
-import {Message} from 'discord.js';
-import fetch from 'node-fetch';
 
 // eslint-disable-next-line prettier/prettier
-export const determineUrl = (url: string): 'youtubeShorts' | 'youtube' | 'tiktok' | 'unknown' => {
-  const tiktokRegex =
-    // eslint-disable-next-line no-irregular-whitespace
-    /http(?:s?):\/\/(?:www\.)?tiktok.com(\/t\/|)([\w\-_]*)(&(amp;)?‌​[\w?‌​=]*)?\/(\?k=\d)/;
-
-  const youtubeRegex =
-    // eslint-disable-next-line no-irregular-whitespace
-    /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-_]*)(&(amp;)?‌​[\w?‌​=]*)?/;
-
+export const determineUrl = (url: string): 'youtubeShorts' | 'unknown' => {
   const youtubeShortRegex =
     // eslint-disable-next-line no-irregular-whitespace
     /http(?:s?):\/\/(?:www\.)?youtube\.com\/shorts\/([\w\-_]*)(\?feature=share)/g;
 
-  if (tiktokRegex.test(url)) {
-    return 'tiktok';
-  }
-  if (youtubeRegex.test(url)) {
-    return 'youtube';
-  }
   if (youtubeShortRegex.test(url)) {
     return 'youtubeShorts';
   }
@@ -30,19 +14,15 @@ export const determineUrl = (url: string): 'youtubeShorts' | 'youtube' | 'tiktok
 };
 
 // eslint-disable-next-line prettier/prettier
-export const downloadVideo = async (url: string, msg: Message): Promise<string> => {
+export const downloadVideo = async (url: string): Promise<string> => {
   // eslint-disable-next-line no-async-promise-executor
   return new Promise(async (resolve, reject) => {
     const randomString = Math.random().toString(36).substring(7);
     const fileName = `${randomString}`;
 
-    // Use node-fetch to get the redirected tiktok URL
-    const res = await fetch(url);
-    const redirectedUrl = res.url;
-
     youtubedl
-      .default(redirectedUrl, {
-        referer: redirectedUrl,
+      .default(url, {
+        referer: url,
         maxFilesize: '8M',
         format: 'mp4',
         output: `${__dirname}/../../videos/${fileName}.%(ext)s`,
